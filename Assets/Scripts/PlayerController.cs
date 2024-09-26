74e7849328cc31739a048ac66cd57746755c748f
YoshiiVT <YoshiiUkichiro@outlook.com>
2024-09-26 11:59:42 +1000

    Week 1 - Player Movement
    Homework:
    
    Void vs Int

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    public float jumpSpeed = 1.0f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        //Gets the rigidbody component attached to this GameObject
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        //Store the horizontal axis value in a float
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        //Store the Vertical axis value in a float
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Create a new Vector3 based on the horizontal and vertical values 
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        //Add force to our rigidbody from our movement vectore * Speed variable
+        rb.AddForce(movement * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+
+            rb.AddForce(new Vector3(0, jumpSpeed, 0));
+        }
+    }
+
+}
+
+//<This is called a comment, the system completely ignores this.
+//Variables = Something that will change
+//Constant = Something that never changes
+//For Games, variables are the building blocks
+//Variables are the first thing in the scrips
+//Int=Interger (Which is a whole number)
+//Float is a number that has decibles (Floating point numbers)
+//String is "alpha numeric characters" which is what is displayed to players
+// (;) means that it is the end of the sentence or command)
+// Vector is the xyz of a world
+// C# will give you a red line where it thinks the error is 
+//Functions (Methods) are another building Blocks
+// Homework Study Void/Int
