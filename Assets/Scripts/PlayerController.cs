ff13946a8ce842fe3cecf911308c556deef73e4b
YoshiiVT <YoshiiUkichiro@outlook.com>
2024-10-10 12:59:58 +1100

    Added UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,7 +11,18 @@ public class PlayerController : MonoBehaviour
     public float jumpSpeed = 1.0f;
     private Rigidbody rb;
     private int pickupCount;
+    private int totalPickups;
+    private float pickupChunk;
     private Timer timer;
+    private bool gameOver = false;
+
+    [Header("UI")]
+    public TMP_Text pickupText;
+    public TMP_Text timerText;
+    public Image pickupImage;
+    public GameObject winPanel;
+    public TMP_Text winTimeText;
+    public GameObject inGamePanel;
 
     void Start()
     {
@@ -17,15 +30,31 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         //Gets the number of pickups in our scene
         pickupCount = GameObject.FindGameObjectsWithTag("Pickup").Length;
+        //Assign the toal amount of pickups
+        totalPickups = pickupCount;
+        //Set the pickup Image fill amount to 0
+        pickupImage.fillAmount = 0;
+        //work out the pickup chunk fill amount
+        pickupChunk = 1.0f / totalPickups;
         //Run the CheckPickups Function
         CheckPickups();
         //Gets the timer object
         timer = FindObjectOfType<Timer>();
         timer.StartTimer();
+
+        //Turn off our win panel
+        winPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        timerText.text = "Time: " + timer.currentTime.ToString("F2"); 
     }
 
     void FixedUpdate()
     {
+        if (gameOver == true)
+            return;
         //Store the horizontal axis value in a float
         float moveHorizontal = Input.GetAxis("Horizontal");
         //Store the Vertical axis value in a float
@@ -51,15 +80,20 @@ public class PlayerController : MonoBehaviour
             Destroy(other.gameObject);
             //Decrement the pickup count
             pickupCount--;
+            // increase the fill amount of our pickup image
+            pickupImage.fillAmount = pickupImage.fillAmount + pickupChunk;
             // Run the Check Pickups Function
             CheckPickups();
-        }
 
+        }
     }
     private void CheckPickups()
     {
+       
+        //Do text stuff
         print("Pickups left: " + pickupCount);
-        if(pickupCount == 0)
+        pickupText.text = "Pickups left: " + pickupCount.ToString();
+        if (pickupCount == 0)
         {
             Wingame();
         }
@@ -67,8 +101,32 @@ public class PlayerController : MonoBehaviour
 
     private void Wingame()
     {
+        //Sets game over to true
+        gameOver = true;
+
+        //Stop the timer
         timer.StopTimer();  
         print("SUGOI!! You win! Your time was: " + timer.GetTime().ToString("F2") + " Seconds");
+
+        //Display the timer on our win time text
+        winTimeText.text = "Time: " + timer.currentTime.ToString("F2");
+
+        //Turn on our win panel
+        winPanel.SetActive(true);
+
+        //Turn off our in game panel
+        inGamePanel.SetActive(false);
+
+        //Stop the ball from rolling
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        
+    }
+
+    //Temporary restart function
+    public void ResetGame()
+    {
+       UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
 }
 
