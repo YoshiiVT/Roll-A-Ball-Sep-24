346f1947ecebc41fb3061a238e5c7909be111d20
YoshiiVT <YoshiiUkichiro@outlook.com>
2024-11-28 19:21:55 +1100

    Level Select Menu + World Tilt Mode

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ProBuilder;
+
+public enum ControlType { Normal, Worldtilt }
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public ControlType controlType;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    public void ToggleWorldTilt(bool _titlt)
+    {
+        if (_titlt) 
+            controlType = ControlType.Worldtilt;
+        else 
+            controlType = ControlType.Normal;
+   
+    }
+}
---
+++ Assets/Scripts/TiltControls.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltControls : MonoBehaviour
+{
+    public float speed = 20f;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+   private void FixedUpdate()
+   {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 moveVector = new Vector3(moveVertical * speed, 0, -moveHorizontal * speed);
+
+   }
+}
